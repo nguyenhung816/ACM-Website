fdf7cb540a166878017633e32ab974be6e9d4deb
n2ai <longhai2511@gmail.com>
2023-03-30 22:54:00 -0500

    Add homepage

--- app/routes.py
+++ app/routes.py
@@ -1,45 +1,16 @@
 from app import app
-from flask import render_template, flash, redirect, url_for, jsonify, Response, json, request
+from flask import render_template, flash
 from app.forms import SignupForm, EmailError
 import random
 from app import db
 from app.models import  UserPassword, UserAccount
-import jwt
-from .database import db
-from datetime import datetime
-from functools import wraps
-
-#A token require for other route
-def token_required(f):
-    @wraps(f)
-    def Token_check(*args, **kwargs):
-        token = None
-
-        if 'x-access-token' in request.headers:
-            token = request.headers['x-access-token']
-
-        if not token:
-            return jsonify({'message' : 'Token is missing!'}), 401
-
-        try: 
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = UserAccount.query.filter_by(email=data['email']).first()
-        except:
-            return jsonify({'message' : 'Token is invalid!'}), 401
-
-        return f(current_user, *args, **kwargs)
-
-    return Token_check
-#Put this under login routen need a secret key
-token = jwt.encode({'email' : UserAccount.email, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=180)}, app.config['SECRET_KEY'])
 
 with app.app_context():
     db.create_all()
 
 @app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+def homepage():
+    return render_template('homepage.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -68,7 +39,7 @@ def signup():
 
         # flash a success message and redirect to index page
         flash('You have successfully signed up!')
-        return render_template('index.html', form=form)
+        return render_template('homepage.html', form=form)
      
     # if the email is already in the database, flash an error message
     elif form.email.errors == [EmailError.EMAIL_IN_DB.value]:
---
+++ app/static/scripts/homepage.js
@@ -0,0 +1,9 @@
+//Hai Trinh code on "Our Stories section 3-30-2023"
+
+$(document).ready(()=>{
+    const scrollbarStaff = $(".scrollbar-staff")
+
+    scrollbarStaff.on("click", ()=>{
+        window.location.replace("")
+    })
+})
